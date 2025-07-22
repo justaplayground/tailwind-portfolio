8f15519eede6bcf6094f5533c40c9b0c982fa49d
giang-tc <158981273+giang-tourchain@users.noreply.github.com>
2025-07-22 15:55:58 +0700

    Add gh action for cicd

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    host: "::",
+    host: '::',
     port: 8080,
   },
   plugins: [react(), tailwindcss()],
