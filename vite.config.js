01418a524ef7129c644fc37ed379e7ccd590325d
giang-tc <158981273+giang-tourchain@users.noreply.github.com>
2025-07-30 17:42:50 +0700

    Update build config

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
   server: {
     host: '::',
     port: 80,
+    hmr: {
+      overlay: false,
+    },
   },
   plugins: [react(), tailwindcss()],
   exclude: ['ref/*'],
@@ -15,5 +18,49 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          animations: ['framer-motion'],
+          utils: ['clsx', 'dayjs', 'tailwind-merge'],
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.')
+          const ext = info[info.length - 1]
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
+            return `assets/images/[name]-[hash].${ext}`
+          }
+          if (/css/i.test(ext)) {
+            return `assets/css/[name]-[hash].${ext}`
+          }
+          return `assets/[name]-[hash].${ext}`
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+    reportCompressedSize: false,
+  },
+  optimizeDeps: {
+    include: [
+      'react',
+      'react-dom',
+      'react-router-dom',
+      'framer-motion',
+      'clsx',
+      'dayjs',
+      'tailwind-merge',
+    ],
+  },
+  css: {
+    devSourcemap: false,
   },
 })
